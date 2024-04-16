698088598c7783a6649bd043ee94b548be3a6127
Santiago Castillo <castillosantiago442@gmail.com>
2024-04-16 18:55:34 -0500

    master: primer commit en master

---
+++ src/main/java/co/ucentral/sistemas/gestionCitasBancarias/GestionCitasBancariasApplication.java
@@ -0,0 +1,13 @@
+package co.ucentral.sistemas.gestionCitasBancarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCitasBancariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCitasBancariasApplication.class, args);
+	}
+
+}
