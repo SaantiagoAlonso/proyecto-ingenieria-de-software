d1790a260696f1028172f1934330025185047368
Santiago <castillosantiago442@gmail.com>
2024-04-23 19:53:43 -0500

    00: modificacion de entidades y creacion de dto's

---
+++ src/main/java/co/ucentral/sistemas/gestionCitasBancarias/dto/SedeDto.java
@@ -0,0 +1,24 @@
+package co.ucentral.sistemas.gestionCitasBancarias.dto;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SedeDto {
+
+
+    private long id_sede;
+
+
+    private String nombre;
+
+
+    private String direccion;
+
+
+
+}
