a369fd6a2611edcc70e3c109c581e6cbcc53777b
SaantiagoAlonso <castillosantiago442@gmail.com>
2024-04-28 14:16:32 -0500

    CU-01-santiago-se realizo el metodo para registrar clientes y los metodos correspondientes en el controlador para mostrar el formulario de registro y se modifico la entidad cliente para que la identificacion no fuera autoincremental

---
+++ src/main/java/co/ucentral/sistemas/gestionCitasBancarias/operaciones/Operaciones.java
@@ -0,0 +1,8 @@
+package co.ucentral.sistemas.gestionCitasBancarias.operaciones;
+
+import co.ucentral.sistemas.gestionCitasBancarias.dto.ClienteDto;
+
+public interface Operaciones {
+
+    public int ingresarCliente(ClienteDto cliente);
+}
